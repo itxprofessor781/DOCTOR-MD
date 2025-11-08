4202ef987a75e4fe0ead33d243fe5a95468d08f8
Doctor Zain <mrrzmode46@gmail.com>
2025-11-08 21:05:43 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 // 🌟 Bot Configuration File 🌟
-// Edit these values to personalize your ARSLAN BOT
+// Edit these values to personalize your DOCTOR BOT
 
 const settings = {
   // 🏷️ Sticker Settings
-  packname: '💎 ARSLAN-TECH-MD 💎',
-  author: '👑 ArslanMD Official 🇵🇰',
+  packname: '🎀 DOCTOR-MD 🎀',
+  author: '👑 Doctormd Official 🇵🇰',
 
   // 🤖 Bot Identity
-  botName: '⚡ ARSLAN TECH MD ⚡',
-  botOwner: '🌍 ArslanMD Official 🇵🇰',   // Change to your name if needed
-  ownerNumber: '923237045919',        // Your number without + symbol
+  botName: '⚡ DOCTOR MD ⚡',
+  botOwner: '🌍 Rana Zain Official 🇵🇰',   // Change to your name if needed
+  ownerNumber: '923360679553',        // Your number without + symbol
 
   // 🎬 APIs
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // For GIF commands
@@ -18,7 +18,7 @@ const settings = {
   commandMode: 'public', // Options: 'public' (everyone) | 'private' (owner only)
 
   // 📝 Meta Information
-  description: '💬 A fun, powerful & stylish WhatsApp MD bot created with ❤️ by ARSLAN POWER.',
+  description: '💬 A fun, powerful & stylish WhatsApp MD bot created with ❤️ by DOCTOR POWER.',
   version: '2.0.0',
 updateZipUrl: "https://github.com/Arslan-MD/Arslan-Tech-MD/archive/refs/heads/main.zip",
   
